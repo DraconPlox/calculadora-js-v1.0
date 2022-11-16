0060a30ebe29614d46bb0b4c100b813d21e47428
DraconPlox <yeray2004@gmail.com>
2022-11-16 19:29:54 +0100

    Añadir funcionalidad a los botones
    Añadir funciones a los botones numericos y botón C.

---
+++ script.js
@@ -0,0 +1,33 @@
+function cero(){
+    document.getElementById("texto").innerHTML = "0";
+}
+function uno(){
+    document.getElementById("texto").innerHTML = "1";
+}
+function dos(){
+    document.getElementById("texto").innerHTML = "2";
+}
+function tres(){
+    document.getElementById("texto").innerHTML = "3";
+}
+function cuatro(){
+    document.getElementById("texto").innerHTML = "4";
+}
+function cinco(){
+    document.getElementById("texto").innerHTML = "5";
+}
+function seis(){
+    document.getElementById("texto").innerHTML = "6";
+}
+function siete(){
+    document.getElementById("texto").innerHTML = "7";
+}
+function ocho(){
+    document.getElementById("texto").innerHTML = "8";
+}
+function nueve(){
+    document.getElementById("texto").innerHTML = "9";
+}
+function borrar_todo(){
+    document.getElementById("texto").innerHTML = " ";
+}
